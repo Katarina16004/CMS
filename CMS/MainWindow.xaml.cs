7cdfe2eb0c165c43be33e8ff6865f570fecca3e3
Katarina16004 <veljkovickatarina56@gmail.com>
2025-07-15 21:18:59 +0200

    povezivanje Login i Menu prozora, sredjivanje dizajna i vidljivost lozinke

--- CMS/MainWindow.xaml.cs
+++ CMS/MainWindow.xaml.cs
@@ -16,9 +16,62 @@ namespace CMS
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool passwordVisibility;
         public MainWindow()
         {
             InitializeComponent();
+            UsernameTextBox.Focus();
+            passwordVisibility = false;
+            PasswordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            EyeIcon.Visibility = Visibility.Collapsed;
+            UsernameTextBox.Focus();
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Menu menuWindow = new Menu();
+            menuWindow.Show();
+            this.Close();
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EyeIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (passwordVisibility)
+            {
+                PasswordTextBox.Visibility = Visibility.Collapsed;
+                PasswordBox.Visibility = Visibility.Visible;
+                PasswordBox.Password = PasswordTextBox.Text;
+
+                EyeIcon.Source = new BitmapImage(new System.Uri("Resources/otvoreno_oko.png", System.UriKind.Relative));
+            }
+            else
+            {
+                PasswordTextBox.Visibility = Visibility.Visible;
+                PasswordBox.Visibility = Visibility.Collapsed;
+                PasswordTextBox.Text = PasswordBox.Password;
+                PasswordTextBox.Focus();
+                PasswordTextBox.CaretIndex = PasswordTextBox.Text.Length;
+
+                EyeIcon.Source = new BitmapImage(new System.Uri("Resources/zatvoreno_oko.png", System.UriKind.Relative));
+            }
+
+            passwordVisibility = !passwordVisibility;
+
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Password))
+                EyeIcon.Visibility = Visibility.Collapsed;
+            else
+                EyeIcon.Visibility = Visibility.Visible;
         }
     }
 }
